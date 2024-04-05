f6c07eb064285e57348948cd0dfe2080b94a13a9
RomaSamkov <Romansamkov.rs@gmail.com>
2024-04-05 19:01:39 +0300

    Update Movies.tsx

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,5 +1,35 @@
-function Movies() {
-  return <div>Movies</div>;
+import { connect } from "react-redux";
+import { Movie } from "../../reducers/movies";
+import { RootState } from "../../store";
+import MoviesCard from "./MoviesCard";
+import "./Movies.css";
+
+interface MoviesProps {
+  movies: Movie[];
 }
 
-export default Movies;
+// eslint-disable-next-line react-refresh/only-export-components
+function Movies({ movies }: MoviesProps) {
+  return (
+    <section>
+      <div className="Movies-list">
+        {movies.map((movie) => (
+          <MoviesCard
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            popularity={movie.popularity}
+            overview={movie.overview}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+const mapStateToProps = (state: RootState) => ({ movies: state.movies.top });
+
+const connector = connect(mapStateToProps);
+
+// eslint-disable-next-line react-refresh/only-export-components
+export default connector(Movies);

655d894b4dad26551663d91a3f474cfa2c8677bd
RomaSamkov <Romansamkov.rs@gmail.com>
2024-04-08 12:45:02 +0300

    Update Movies.tsx

--- src/features/Movies/Movies.tsx
+++ src/features/Movies/Movies.tsx
@@ -1,14 +1,35 @@
-import { connect } from "react-redux";
-import { Movie } from "../../reducers/movies";
+import { connect, useDispatch } from "react-redux";
+import { Movie, moviesLoaded } from "../../reducers/movies";
 import { RootState } from "../../store";
 import MoviesCard from "./MoviesCard";
 import styles from "./Movies.module.scss";
+import { useEffect } from "react";
+import { client } from "../../api/tmdb";
 
 interface MoviesProps {
   movies: Movie[];
 }
 
 function Movies({ movies }: MoviesProps) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    async function loadData() {
+      const config = await client.getConfiguration();
+      const imageUrl = config.images.base_url;
+      const results = await client.getNowPlaying();
+      const mappedResults: Movie[] = results.map((m) => ({
+        id: m.id,
+        title: m.title,
+        popularity: m.popularity,
+        overview: m.overview,
+        image: m.backdrop_path
+          ? `${imageUrl}w780${m.backdrop_path}`
+          : undefined,
+      }));
+      dispatch(moviesLoaded(mappedResults));
+    }
+    loadData();
+  }, [dispatch]);
   return (
     <section>
       <div className={styles.list}>
@@ -19,6 +40,7 @@ function Movies({ movies }: MoviesProps) {
             title={movie.title}
             popularity={movie.popularity}
             overview={movie.overview}
+            image={movie.image}
           />
         ))}
       </div>
